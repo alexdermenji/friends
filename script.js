5bc40ad9d81d264758a8bcd8ba78161a6ef8f7fe
alexdermenji <allexdermenji@gmail.com>
2021-06-03 21:35:06 +0100

    functionality move elements done

--- script.js
+++ script.js
@@ -25,27 +25,67 @@ function callApi(method, params) {
   });
 }
 
+function moveFriend(friendsToRemove, friendsToAdd, friendId) {
+  const friendIndex = friendsToRemove.findIndex(
+    (friend) => friend.id === friendId
+  );
+
+  const deletedFriends = friendsToRemove.splice(friendIndex, 1);
+  friendsToAdd.unshift(deletedFriends[0]);
+}
+
+const input = document.querySelector(".input");
+
+input.addEventListener("keyup", (e) => {
+  // TODO: check e.target.closest(".friends").classList.contains("friends--best")
+  // TODO: move to separate function
+  const filteredFriends = friends.filter(
+    (friend) =>
+      friend.first_name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+      friend.last_name.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  console.log(friends);
+  render(filteredFriends, "");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    const parent = e.target.closest(".friend");
+    const friendId = +parent.getAttribute("data-id");
+    if (e.target.closest(".friends").classList.contains("friends--best")) {
+      moveFriend(bestFriends, friends, friendId);
+    } else {
+      moveFriend(friends, bestFriends, friendId);
+    }
+
+    // TODO: ??? apply filter on friends and bestFriends
+    render(friends, "");
+    render(bestFriends, ".friends--best");
+  }
+});
+
+function render(friendsArr, selector) {
+  const template = document.querySelector("#user-template").textContent;
+  const render = Handlebars.compile(template);
+  const html = render({ items: friendsArr });
+  const results = document.querySelector(`${selector} .friends__list`);
+  results.innerHTML = html;
+}
+
+let friends = [];
+let bestFriends = [];
+
 (async () => {
   try {
     await auth();
     const [me] = await callApi("users.get", { name_case: "gen" });
     const title = document.querySelector(".friends__title");
     title.textContent = `Друзья ${me.first_name} ${me.last_name}`;
-    const friends = await callApi("friends.get", {
+    const friendsApi = await callApi("friends.get", {
       fields: "city, country, photo_100",
     });
-    const template = document.querySelector("#user-template").textContent;
-    const render = Handlebars.compile(template);
-    const html = render(friends);
-    const results = document.querySelector(".friends__list");
-    results.innerHTML = html;
-
-    document.addEventListener("click", (e) => {
-      if (e.target.classList.contains("btn")) {
-        const parent = e.target.closest(".friend");
-        parent.remove();
-      }
-    });
+    friends = friendsApi.items;
+    render(friends, "");
   } catch (e) {
     console.error(e);
   }
